42df27105636314fa39ce37ae555daa197910190
Juan Jose Galvin <juanjosegr9@gmail.com>
2024-10-21 10:17:43 +0000

    arreglos

--- macros/insert_event_values.sql
+++ macros/insert_event_values.sql
@@ -1,4 +1,7 @@
 insert into events values
-(1,'Black Friday','11/24 - 11/30' ),
-(2,'Cyber Monday', '12/2'),
-(3,'Navidad','12/18 12/25' )
+(1, 'Black Friday', '2000-11-24', '2000-11-30', null),
+(2, 'Cyber Monday', '2000-12-02', '2000-12-02', null),
+(3, 'Navidad', '2000-12-18', '2000-12-25', null ),
+(4, 'Independece Day', '2000-07-04', '2000-07-04', 24),
+(5, 'Rebajas de invierno', '2000-01-10', '2000-02-20', 6),
+(6, 'Boxing Day', '2000-12-26', '2000-12-26', 23 )
