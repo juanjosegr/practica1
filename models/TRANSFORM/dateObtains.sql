7715137b60f630e3bf51213d89a59a1bea5c447e
Juan Jose Galvin <juanjosegr9@gmail.com>
2024-10-23 12:29:38 +0000

    Cambios en dateobtains y creacion de otra tabla

--- models/TRANSFORM/dateObtains.sql
+++ models/TRANSFORM/dateObtains.sql
@@ -21,7 +21,9 @@ with v1 as(
             when datediff('day', l.RECEIPTDATE, L.COMMITDATE) <= 0 then '1'-- En plazo, el pedido se ha entregado como muy tarde en la fecha estimada
             when datediff('day', l.RECEIPTDATE, L.COMMITDATE) <= 10 then '2'--Entrega tardía, el pedido se ha entregado como mucho 10 días después de la fecha estimada
         end as ID_PLAZO_ENTREGA,
-        e.desc as evento
+        e.desc as evento,
+        c.nation_key as nation,
+        r.region_key as region
     from
         raw.DBT_JUANJOSEGR.orders o
     right join 
@@ -57,12 +59,13 @@ with v1 as(
     where 
         l.OPERATION != 'A'
     group by
-        o.ORDER_DATE, c.CUSTOMER_KEY, l.ORDER_KEY,  p.PART_NAME, l.QUANTITY, l.PRICE, p.PART_KEY, l.OPERATION, r.REGION_KEY, cr.RATE_CURRENCY, CR.CURRENCY, l.RECEIPTDATE, L.COMMITDATE, c.NATION_KEY, cr.UTC_HOUR_CONVER, e.desc
+        o.ORDER_DATE, c.CUSTOMER_KEY, l.ORDER_KEY,  p.PART_NAME, l.QUANTITY, l.PRICE, p.PART_KEY, l.OPERATION, r.REGION_KEY, cr.RATE_CURRENCY, CR.CURRENCY, l.RECEIPTDATE, L.COMMITDATE, c.NATION_KEY, cr.UTC_HOUR_CONVER, e.desc, r.region_key
     order by
         c.CUSTOMER_KEY,o.ORDER_DATE,  p.PART_KEY
 ), v2 as(
     select
         l.order_key,
+        sh.shop_key,
         round((l.price * cr.rate_currency),2) || ' ' || cr.currency as shop_price,
         o.ORDER_DATE || ' 00:00:00'  as orderDate,
         dateadd (hour, cr.UTC_HOUR_CONVER, orderDate ) as shop_hours
@@ -80,11 +83,12 @@ with v1 as(
     join
         raw.DBT_JUANJOSEGR.converion_rates cr on
         n.nation_key = cr.nation_key
-    group by l.order_key, l.price, cr.rate_currency, cr.currency, o.ORDER_DATE, cr.UTC_HOUR_CONVER
+    group by l.order_key, l.price, cr.rate_currency, cr.currency, o.ORDER_DATE, cr.UTC_HOUR_CONVER, sh.shop_key
     order by l.order_key
 )
 select
     v1.CUSTOMER_KEY,
+    v2.shop_key,
     v1.ORDER_KEY,
     v1.PART_KEY,
     v1.PART_NAME,
@@ -97,10 +101,10 @@ select
     v1.customer_hours,
     v2.shop_hours,
     v1.ID_PLAZO_ENTREGA,
-    v1.evento
+    v1.evento,
 from 
     v1
 join v2 on
     v1.order_key = v2.order_key
-group by v1.CUSTOMER_KEY, v1.ORDER_KEY, v1.PART_KEY, v1.PART_NAME, v1.Operation, v1.QUANTITY, v1.PRICE, v1.customer_price, v2.shop_price, v1.orderDate, v1.customer_hours, v2.shop_hours, v1.ID_PLAZO_ENTREGA,      v1.evento    
+group by v1.CUSTOMER_KEY, v1.ORDER_KEY, v1.PART_KEY, v1.PART_NAME, v1.Operation, v1.QUANTITY, v1.PRICE, v1.customer_price, v2.shop_price, v1.orderDate, v1.customer_hours, v2.shop_hours, v1.ID_PLAZO_ENTREGA,      v1.evento , v2.shop_key
 order by v1.customer_key
